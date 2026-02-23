05a7c7b3af751550f5af62669fe97656c6b2050f
alinajibi1234 <eaa24anaj@students.eaaa.dk>
2026-02-23 11:36:58 +0100

    Update Program.cs
    d

--- Program.cs
+++ Program.cs
@@ -4,23 +4,32 @@ using RedditApi.Models;
 using RedditApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-// Sætter CORS så API'en kan bruges fra andre domæner
+// --- 1. REGISTRER SERVICES (Altid FØR builder.Build()) ---
+
 var AllowSomeStuff = "_AllowSomeStuff";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: AllowSomeStuff, builder => {
-        builder.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+    options.AddPolicy(name: AllowSomeStuff, policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
-// Tilføj DbContext factory som service.
+// Tilføj DbContext
 builder.Services.AddDbContext<RedditContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ContextSQLite")));
 
+// --- 2. BYG APPEN (Låser for flere services) ---
+var app = builder.Build();
+
+// --- 3. MIDDLEWARE PIPELINE (Konfiguration af hvordan appen kører) ---
+
+// VIGTIGT: Aktivér CORS policy
+app.UseCors(AllowSomeStuff);
+
+// Midlertidige data til GET endpointet
 var users = new List<User>
 {
     new User { UserId = 1, Username = "KodningsHej" },
@@ -36,47 +45,39 @@ var posts = new List<Post>
         Content = "Det her kører bare!", 
         Upvote = 10, 
         UserId = 1,
-        User = users[0],
-        Comments = new List<Comment> { 
-            new Comment { CommentId = 1, Text = "Godt gået!", Upvote = 2 } 
-        }
-    },
-    new Post 
-    { 
-        PostId = 2, 
-        Title = "Hvorfor bruge modeller?", 
-        Content = "Fordi det giver struktur!", 
-        Upvote = 50, 
-        UserId = 2,
-        User = users[1]
+        User = users[0]
     }
 };
+
+// --- 4. ENDPOINTS ---
+
+app.MapGet("/", () => "Morten er en fkn Nuddelspiser!");
 
 app.MapGet("/api/posts", () =>
 {
     return Results.Ok(posts);
 });
 
-app.MapGet("/", () => "Morten er en fkn Nuddelspiser!");
-
-//Post: Opret en ny post på siden
+// Post: Opret en ny post i databasen
 app.MapPost("/api/posts", (RedditContext db, Post post) =>
 {
     db.Posts.Add(post);
     db.SaveChanges();
+    return Results.Created($"/api/posts/{post.PostId}", post);
 });
 
 app.MapPost("/api/posts/{id}/comments", (RedditContext db, int id, Comment comment) =>
 {
-    //Tilføjelse af resten af koden Ali!
+    // Her kan Ali skrive sin kode
+    return Results.Ok("Kommentar modtaget");
 });
 
-
-
-//Seed Data
+// --- 5. DATABASE INITIALISERING (Seed) ---
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<RedditContext>();
+    db.Database.EnsureCreated(); // Opretter databasen hvis den ikke findes
 }
 
 app.Run();

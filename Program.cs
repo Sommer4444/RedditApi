43a2dd63eb471162c7bf7ebd365a2a3349ca36a7
alinajibi1234 <eaa24anaj@students.eaaa.dk>
2026-02-23 11:01:50 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
+using RedditApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var users = new List<User>
+{
+    new User { UserId = 1, Username = "KodningsHej" },
+    new User { UserId = 2, Username = "DotNetFan" }
+};
+
+var posts = new List<Post>
+{
+    new Post 
+    { 
+        PostId = 1, 
+        Title = "Min første API", 
+        Content = "Det her kører bare!", 
+        Upvote = 10, 
+        UserId = 1,
+        User = users[0],
+        Comments = new List<Comment> { 
+            new Comment { CommentId = 1, Text = "Godt gået!", Upvote = 2 } 
+        }
+    },
+    new Post 
+    { 
+        PostId = 2, 
+        Title = "Hvorfor bruge modeller?", 
+        Content = "Fordi det giver struktur!", 
+        Upvote = 50, 
+        UserId = 2,
+        User = users[1]
+    }
+};
+
+app.MapGet("/api/posts", () =>
+{
+    return Results.Ok(posts);
+});
 
 app.Run();

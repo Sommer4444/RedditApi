c0f2eac903b31a6257468e0c9613a40fd86dd051
MortenHHD <eaa24mhhda@students.eaaa.dk>
2026-02-23 11:03:59 +0100

    fixed main
    fixed main

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,15 +7,10 @@ public class Comment
     public int Upvote { get; set; }
     public int Downvote { get; set; }
     
-<<<<<<< HEAD
-    //Så vi ved hvem der kommenterer
-=======
-    // Relation tilbage til tråden (Post)
     public int PostId { get; set; }
     public Post Post { get; set; }
     
     // Relation til user af kommentaren
     public int UserId { get; set; }
->>>>>>> e9f0732efe1b08cacd7850c57f65c09a736e7fff
     public User User { get; set; }
 }

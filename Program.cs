cc069363d0c5bbf66ed05e58ba28ef95a7f3fbde
MortenHHD <eaa24mhhda@students.eaaa.dk>
2026-02-23 11:11:14 +0100

    Cors + DbContext som factory
    Cors + DbContext som factory

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Json;
 using RedditApi.Models;
 using RedditApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Sætter CORS så API'en kan bruges fra andre domæner
+var AllowSomeStuff = "_AllowSomeStuff";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSomeStuff, builder => {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+// Tilføj DbContext factory som service.
+builder.Services.AddDbContext<RedditContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ContextSQLite")));
+
+
 app.MapGet("/", () => "Hello World!");
+
+
 
 
 
 //Post: Opret en ny post på siden
 
-app.MapGet("/api/posts", (RedditContext db, Post post) =>
+app.MapPost("/api/posts", (RedditContext db, Post post) =>
 {
     db.Posts.Add(post);
     db.SaveChanges();

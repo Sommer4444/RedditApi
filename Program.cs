e7332fb4d02cbfa4ceddfc7baa1524210a0923db
alinajibi1234 <eaa24anaj@students.eaaa.dk>
2026-02-23 11:25:41 +0100

    Update Program.cs
    mock og get

--- Program.cs
+++ Program.cs
@@ -21,8 +21,43 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<RedditContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ContextSQLite")));
 
+var users = new List<User>
+{
+    new User { UserId = 1, Username = "KodningsHej" },
+    new User { UserId = 2, Username = "DotNetFan" }
+};
 
-app.MapGet("/", () => "Hello World!");
+var posts = new List<Post>
+{
+    new Post 
+    { 
+        PostId = 1, 
+        Title = "Min første API", 
+        Content = "Det her kører bare!", 
+        Upvote = 10, 
+        UserId = 1,
+        User = users[0],
+        Comments = new List<Comment> { 
+            new Comment { CommentId = 1, Text = "Godt gået!", Upvote = 2 } 
+        }
+    },
+    new Post 
+    { 
+        PostId = 2, 
+        Title = "Hvorfor bruge modeller?", 
+        Content = "Fordi det giver struktur!", 
+        Upvote = 50, 
+        UserId = 2,
+        User = users[1]
+    }
+};
+
+app.MapGet("/api/posts", () =>
+{
+    return Results.Ok(posts);
+});
+
+app.MapGet("/", () => "Morten er en fkn Nuddelspiser!");
 
 //Post: Opret en ny post på siden
 app.MapPost("/api/posts", (RedditContext db, Post post) =>

2a9d10a4eb09181209982f4bb470bf3ae9f1987a
Sommer4444 <eaa24ksom@students.eaaa.dk>
2026-02-23 10:27:03 +0100

    Relationer
    Tilføjet relationer

--- Models/Post.cs
+++ Models/Post.cs
@@ -7,4 +7,11 @@ public class Post
     public string Content { get; set; }
     public int Upvote { get; set; }
     public int Downvote { get; set; }
+    
+    // Relation til forfatteren
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
+    // Relation til kommentarer
+    public List<Comment> Comments { get; set; } = new();
 }
